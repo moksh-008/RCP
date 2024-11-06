ccea1e6949936bb142982d9abbcf73ec6f76e62b
mokshpatel <oc22442@bristol.ac.uk>
2024-11-06 15:12:55 +0000

    With the solutions

--- secretstrings/client/client.go
+++ secretstrings/client/client.go
@@ -1,16 +1,38 @@
 package main
+
 import (
-//	"net/rpc"
+	"bufio"
+	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
+
+	//	"net/rpc"
 	"flag"
-//	"bufio"
-//	"os"
-//	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
+	"net/rpc"
+	"os"
+
+	//	"bufio"
+	//	"os"
+	//	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
 	"fmt"
 )
 
-func main(){
-	server := flag.String("server","127.0.0.1:8030","IP:port string to connect to as server")
+func makeCall(client *rpc.Client, message string) {
+	request := stubs.Request{Message: message}
+	response := new(stubs.Response)
+	client.Call(stubs.ReverseHandler, request, response)
+	fmt.Println("Responded: " + response.Message)
+}
+func main() {
+	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to as server")
 	flag.Parse()
-	fmt.Println("Server: ", *server)
-	//TODO: connect to the RPC server and send the request(s)
+	client, _ := rpc.Dial("tcp", *server)
+	defer client.Close()
+
+	file, _ := os.Open("wordlist")
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		t := scanner.Text()
+		fmt.Println("Called: " + t)
+		makeCall(client, t)
+	}
+
 }
